b08ece186540dbf48392df5cef0c48e9318cd058
tanyaagarwal900 <68996316+tanyaagarwal900@users.noreply.github.com>
2026-03-18 21:53:52 +0530

    Create scd2_merge.sql

---
+++ gold/scd2_merge.sql
@@ -0,0 +1,31 @@
+MERGE INTO dim_customer AS target
+USING staged_updates AS source
+ON target.customer_id = source.customer_id
+AND target.is_current = true
+
+WHEN MATCHED AND source.is_current = false
+THEN UPDATE SET
+target.end_date = source.end_date,
+target.is_current = false
+
+WHEN NOT MATCHED
+THEN INSERT (
+customer_id,
+name,
+email,
+city,
+status,
+start_date,
+end_date,
+is_current
+)
+VALUES (
+source.customer_id,
+source.name,
+source.email,
+source.city,
+source.status,
+source.start_date,
+source.end_date,
+source.is_current
+);
